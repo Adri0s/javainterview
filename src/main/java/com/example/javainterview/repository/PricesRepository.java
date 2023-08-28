8f23ce71031d901a1a4970d44c8136fb9f25b44c
Adrián Torres <torresbadrian@gmail.com>
2023-08-28 20:21:41 +0200

    Repositorio funcionando (sin filtrado de prioridad)

--- src/main/java/com/example/javainterview/repository/PricesRepository.java
+++ src/main/java/com/example/javainterview/repository/PricesRepository.java
@@ -2,8 +2,22 @@ package com.example.javainterview.repository;
 
 import com.example.javainterview.entity.Prices;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface PricesRepository extends JpaRepository<Prices, Long> {
+
+    @Query("FROM Prices p\n" +
+            "WHERE :applicationDate BETWEEN start_date AND end_date\n" +
+            "AND p.product_id=:productId\n" +
+            "AND p.brand.id=:brandId")
+    List<Optional<Prices>> findAllWithoutPriorityCheck(@Param("applicationDate") Date applicationDate,
+                                                       @Param("productId") int productId,
+                                                       @Param("brandId") Long brandId);
 }
---
+++ src/test/java/com/example/javainterview/repository/PricesRepositoryTests.java
@@ -0,0 +1,41 @@
+package com.example.javainterview.repository;
+
+import com.example.javainterview.entity.Prices;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+@DataJpaTest
+public class PricesRepositoryTests {
+
+    private final PricesRepository pricesRepository;
+
+    @Autowired
+    public PricesRepositoryTests(PricesRepository pricesRepository) {
+        this.pricesRepository = pricesRepository;
+    }
+
+    @Test
+    public void givenPricesFilter_whenFindAllWithoutPiorityCheck_thenReturnAllMatchValues() {
+        // given the params
+        LocalDateTime applicationLocalTimeDate = LocalDateTime.parse("2020-06-14T16:00:00");
+        Date applicationDate = Date.from(applicationLocalTimeDate.atZone(ZoneId.systemDefault()).toInstant());
+        int productId = 35455;
+        Long brandId = 1L;
+
+        // find
+        List<Optional<Prices>> allWithoutPriorityCheck = pricesRepository.findAllWithoutPriorityCheck(applicationDate, productId, brandId);
+
+        // then
+        assertThat(allWithoutPriorityCheck).isNotNull();
+        assertThat(!allWithoutPriorityCheck.isEmpty());
+    }
+}
